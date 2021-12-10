8764e898d6991b9787d7804a3b05ce19f379b4d1
Roeck <rons9n@gmail.com>
2021-12-10 14:04:20 +0000

    Card elements with styling

--- client/src/game.js
+++ client/src/game.js
@@ -36,6 +36,18 @@ class Game {
         gameLi.dataset.id = this.id
         gameList.appendChild(gameLi)
 
+        const h3 = document.createElement('h3')
+        h3.className = ("h3")
+        h3.innerText = this.name
+
+        const h6 = document.createElement('h6')
+        p.className = ("card-text")
+        p.innerText = "Producer: " + this.producer + " | " + "Score: " + this.score
+
+        const img = document.createElement('img')
+        img.src = this.image
+        img.width = 130
+
     })
 
     static submitGame(s) {
