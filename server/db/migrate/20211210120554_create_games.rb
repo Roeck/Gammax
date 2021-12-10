da0115161d6b2a01c2daebaaa9395e9d7bfddc27
Roeck <rons9n@gmail.com>
2021-12-10 12:10:33 +0000

    Game attributes and migration

--- server/db/migrate/20211210120554_create_games.rb
+++ server/db/migrate/20211210120554_create_games.rb
@@ -1,6 +1,10 @@
 class CreateGames < ActiveRecord::Migration[6.1]
   def change
     create_table :games do |t|
+      t.string :name
+      t.string :producer
+      t.float :score
+      t.string :image
 
       t.timestamps
     end
