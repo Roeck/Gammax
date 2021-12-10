8d02534ffdfbea6a8172ea9048e9501e5a46438f
Roeck <rons9n@gmail.com>
2021-12-10 12:30:49 +0000

    Comment attribute and migration

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_10_120554) do
+ActiveRecord::Schema.define(version: 2021_12_10_121206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.bigint "game_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_id"], name: "index_comments_on_game_id"
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "name"
@@ -24,4 +32,5 @@ ActiveRecord::Schema.define(version: 2021_12_10_120554) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "games"
 end
