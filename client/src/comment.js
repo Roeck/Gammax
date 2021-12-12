40d6b138d9e9542896987d9a02090f69a7aae910
Roeck <rons9n@gmail.com>
2021-12-12 06:53:25 -0500

    submitComment function

--- client/src/comment.js
+++ client/src/comment.js
@@ -14,4 +14,25 @@ class Comment {
         const commentList = e.target.previousElementSibling
         const gameId = e.target.parentElement.dataset.id
     }
+
+    static submitComment(commentText, commentList, gameId) {
+
+        fetch(commentsURL, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                },
+                body: JSON.stringify({
+                    text: commentText,
+                    game_id: gameId,
+                })
+            })
+            .then(response => response.json())
+            .then(comment => {
+                let newComment = new Comment(comment)
+                newComment.renderComment(commentList)
+            })
+
+    }
 }
